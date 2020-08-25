24dd13c5f99c14a473e500a5950c25c9b41ce76e
ZephyrGitCode <zephyr.dobson@outlook.com>
2020-08-25 20:06:49 +0930

    Copied project from previous and will be stripping it down to minimum for fresh start
    Signed-off-by: ZephyrGitCode <zephyr.dobson@outlook.com>

---
+++ lib/views/home.php
@@ -0,0 +1,39 @@
+<p><?php echo $message ?></p>
+
+<?php
+// email test
+//mail("zephyr.dobson@outlook.com", "hello", "Hello");
+if(!empty($arts)){
+  echo "<h2>Artworks</h2>";
+  
+  foreach($arts As $art){
+    $artno = htmlspecialchars($art['artNo'],ENT_QUOTES, 'UTF-8');
+    $title = htmlspecialchars($art['title'],ENT_QUOTES, 'UTF-8');
+    $artdesc = htmlspecialchars($art['artdesc'],ENT_QUOTES, 'UTF-8');
+    $price = htmlspecialchars($art['price'],ENT_QUOTES, 'UTF-8');
+    $category = htmlspecialchars($art['category'],ENT_QUOTES, 'UTF-8');
+    $size = htmlspecialchars($art['size'],ENT_QUOTES, 'UTF-8');
+    $image = htmlspecialchars($art['link'],ENT_QUOTES, 'UTF-8');
+    ?>
+    <div class="artlist">
+    <a href="<?php echo "/"."art/"."{$artno}"?>">
+          <div class="dimage">
+            <img href="" src="<?php echo "{$image}"?>" class="artimage"/>
+          </div>
+        </a>
+<?php
+      echo "<li>{$title}, {$artdesc}, {$price}, {$category}, {$size}</li>";
+      ?>
+      <a href="<?php echo "/"."art/"."{$artno}"?>" class="inspect"><p>Inspect</p></a>
+      <?php
+?>
+      </div>
+<?php
+    }
+  }
+  else{
+    echo "<h2>Database Failed to load.</h2>";
+}
+?>
+
+
---
+++ lib/views/partials/form.password.php
@@ -0,0 +1,2 @@
+<label for='password'>Password (6 characters minimum) *</label>
+<input type='password' id='password' name='password' placeholder=""/>
