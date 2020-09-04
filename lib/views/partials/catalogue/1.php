2c27480b0cecf48a339d898e02e1b8a0eebf6048
ChenjianHong2020 <70307859+ChenjianHong2020@users.noreply.github.com>
2020-09-04 17:07:21 +1000

    select vendors page
    select vendors page

---
+++ lib/views/partials/catalogue/1.php
@@ -0,0 +1,5 @@
+<p><?php echo $message ?></p>
+
+<div class="ItemsSelect">
+  <p>Ugly Duckling</p>
+</div>
---
+++ lib/views/partials/catalogue/2.php
@@ -0,0 +1,5 @@
+<p><?php echo $message ?></p>
+
+<div class="ItemsSelect">
+  <p>Luch Room</p>
+</div>
