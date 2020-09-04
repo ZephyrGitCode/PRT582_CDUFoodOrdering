736773575b681e21619dc7ea71598834c0d09559
ChenjianHong2020 <70307859+ChenjianHong2020@users.noreply.github.com>
2020-09-04 17:44:58 +1000

    Update home.php

--- lib/views/home.php
+++ lib/views/home.php
@@ -2,5 +2,15 @@
 
 <div class="VendorSelect">
   <p>Select a vendor please</p>
-  <img src="views/images/uglyduckling.png"/>
+
+  <a href="catalogue/1.php">
+    <img src="lib/views/images/uglyduckling.png" height=300 width=300/>
+  </a>
+  <a href="catalogue/2.php">
+    <img src="lib/views/images/lunchroom.png" height=300 width=300/>
+  </a>
+  <a href="catalogue/3.php">
+    <img src="lib/views/images/arthouse.png" height=300 width=300/>
+  </a>
+
 </div>
